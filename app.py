beb9a3af82ebbe2357cbba773008ae9c02a2ce5a
vielspass777 <gyanarwisdom@gmail.com>
2025-07-05 06:16:01 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def generate_reply(msg):
     return "Sorry, I didn't understand. Try another question?"
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
